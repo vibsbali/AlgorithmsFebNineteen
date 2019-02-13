8043edc6173a48f99301e6f3606aa2441d89c76e
Vaibhav Bali <vbali@hub24.com.au>
2019-02-13 12:59:29 +1100

    Implemented MaxHeap

---
+++ Algorithms.Trees/MaxHeap.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Trees
+{
+    public class MaxHeap<T>
+        where T: IComparable<T>
+    {
+        private IList<T> BackingStore { get; set; }
+	    public int Count { get; private set; }
+        public MaxHeap()
+        {
+            BackingStore = new List<T>();
+        }
+
+	    public T Pop()
+	    {
+		    if (Count <= 0)
+		    {
+			    throw new InvalidOperationException();
+		    }
+
+		    var itemToReturn = BackingStore[0];
+		    --Count;
+		    BackingStore[0] = BackingStore[Count];
+		    BackingStore.RemoveAt(Count); //Same as setting BackingStore[Count] == default(T);
+		    SwimDown(0);
+		    return itemToReturn;
+	    }
+
+	    private void SwimDown(int idx)
+	    {
+		    var left = idx * 2 + 1;
+		    var right = idx * 2 + 2;
+		    var lastIdx = Count - 1;
+
+		    if (left > lastIdx)
+		    {
+			    return;
+		    }
+
+		    int idxToCompare;
+		    if (right <= lastIdx)
+		    {
+			    if (BackingStore[left].CompareTo(BackingStore[right]) <= 0)
+			    {
+				    idxToCompare = right;
+			    }
+			    else
+			    {
+				    idxToCompare = left;
+			    }
+		     }
+		    else
+		    {
+			    idxToCompare = left;
+		    }
+
+		    if (BackingStore[idx].CompareTo(BackingStore[idxToCompare]) < 0)
+		    {
+			    Swap(idx, idxToCompare);
+				SwimDown(idxToCompare);
+		    }
+	    }
+
+	    public void Add(T item)
+        {
+			BackingStore.Add(item);
+	        SwimUp(Count);
+	        ++Count;
+        }
+
+	    private void SwimUp(int idx)
+	    {
+		    var parent = (idx - 1) / 2;
+		    if (parent >= 0 && BackingStore[parent].CompareTo(BackingStore[idx]) < 0)
+		    {
+			    Swap(parent, idx);
+				SwimUp(parent);
+		    }
+	    }
+
+	    private void Swap(int left, int right)
+	    {
+		    var temp = BackingStore[left];
+		    BackingStore[left] = BackingStore[right];
+		    BackingStore[right] = temp;
+	    }
+    }
+
+	
+}
--- ConsoleRunner/Program.cs
+++ ConsoleRunner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using Algorithms.StringSearch;
 using Algorithms.Trees;
 
@@ -8,17 +9,75 @@ namespace ConsoleRunner
    {
       static void Main(string[] args)
       {
-          var trie = new Tries();
-          trie.Add("amex");
-          trie.Add("amir");
-          trie.Add("amritsar");
+         var maxHeap = new MaxHeap<int>();
 
-          var words = trie.FindWords("am");
-          foreach (var word in words)
-          {
-              Console.WriteLine(word);
-          }
+		  maxHeap.Add(3);
+	      if (maxHeap.Pop() != 3)
+	      {
+		      throw new Exception();
+	      }
 
-      }
-   }
+		  maxHeap.Add(5);
+		  maxHeap.Add(6);
+
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+
+	      maxHeap.Add(5);
+	      maxHeap.Add(6);
+	      maxHeap.Add(4);
+
+	      Console.WriteLine();
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+
+
+	      maxHeap.Add(5);
+	      maxHeap.Add(6);
+	      maxHeap.Add(3);
+	      maxHeap.Add(4);
+
+	      Console.WriteLine();
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+
+	      maxHeap.Add(6);
+	      maxHeap.Add(8);
+	      maxHeap.Add(12);
+	      maxHeap.Add(4);
+	      maxHeap.Add(-3);
+
+	      Console.WriteLine();
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+	      Console.WriteLine(maxHeap.Pop());
+
+	      maxHeap.Add(25);
+	      maxHeap.Add(100);
+	      maxHeap.Add(125);
+	      maxHeap.Add(4);
+	      maxHeap.Add(89);
+	      maxHeap.Add(25);
+	      maxHeap.Add(25);
+	      maxHeap.Add(2);
+	      maxHeap.Add(225);
+	      maxHeap.Add(250);
+	      maxHeap.Add(-25);
+	      maxHeap.Add(-12);
+	      maxHeap.Add(160);
+	      maxHeap.Add(1180);
+
+	      Console.WriteLine();
+	      while (maxHeap.Count != 0)
+	      {
+		      Console.WriteLine(maxHeap.Pop());
+		      
+	      }
+		}
+	}
 }
